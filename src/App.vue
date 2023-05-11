e8126a77cd409e4834b1767d85aff9606bd6658c
sakhn <sakhnikarseni@mail.ru>
2023-05-11 19:11:20 +0300

    # добавил шаблончик

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,53 @@
   <v-app>
     <v-app-bar title="BALANCEV GANDON" app>
     </v-app-bar>
+    <v-card
+        class="mx-auto"
+        max-width="344"
+    >
+      <v-card-text>
+        <div>Word of the Day</div>
+        <p class="text-h4 text--primary">
+          be•nev•o•lent
+        </p>
+        <p>adjective</p>
+        <div class="text--primary">
+          well meaning and kindly.<br>
+          "a benevolent smile"
+        </div>
+      </v-card-text>
+      <v-select
+          v-model="test"
+          label="Select"
+          :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+      ></v-select>
+      <v-card-actions>
+        <v-btn
+            border
+            variant="text"
+            color="grey-darken-1"
+        >
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      test: ''
+    }
+  },
+  
+  watch: {
+    test(value) {
+      console.log(value)
+    }
   }
 }
 </script>
